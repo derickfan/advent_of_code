932684e8af98659238e67b162a16c7071adf078e
derickfan <fanderick9999@gmail.com>
2022-12-06 00:45:37 -0500

    day 6

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,10 @@ import util.AdventOfCodeConnector
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val problem = Day5()
+    val problem = Day6()
     val answer = problem.part2()
     println(answer)
-//    AdventOfCodeConnector.submitAnswer(5, 2, answer)
+//    AdventOfCodeConnector.submitAnswer(6,2, answer)
   }
 
 }
